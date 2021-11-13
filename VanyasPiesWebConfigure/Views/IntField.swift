bc72b09d3bf2f4234542a3da935be6c0336d40e0
Ben Schultz <bens4lsu@gmail.com>
2021-11-13 10:32:27 -0600

    bug fixes

---
+++ VanyasPiesWebConfigure/Views/IntField.swift
@@ -0,0 +1,20 @@
+//
+//  IntField.swift
+//  VanyasPiesWebConfigure
+//
+//  Created by Ben Schultz on 9/23/21.
+//
+
+import SwiftUI
+
+struct IntField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct IntField_Previews: PreviewProvider {
+    static var previews: some View {
+        IntField()
+    }
+}
